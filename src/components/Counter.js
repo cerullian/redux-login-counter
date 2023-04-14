e58c4689ce0c49ee964846f8079aee8fdd8e2855
“dpatricio” <dpatricio@ubiwhere.com>
2023-04-14 12:19:37 +0100

    added payload to reducer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -12,6 +12,10 @@ const Counter = () => {
     dispatch({ type: "increment" });
   };
 
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 10 });
+  };
+
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
@@ -24,6 +28,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increate by 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
